31061b98e4f18cd9f54a1625f264423d8ceffe17
Fiza Ahmad <141174715+fizzzzaaa@users.noreply.github.com>
2024-12-01 16:11:28 +0500

    message print
    show login successful message

--- lib/auth_screen.dart
+++ lib/auth_screen.dart
@@ -10,85 +10,162 @@ class _AuthScreenState extends State<AuthScreen> {
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
   final FirebaseAuth _auth = FirebaseAuth.instance;
-  bool _isPasswordVisible = false; // To toggle password visibility
+  bool _isPasswordVisible = false;
 
   // Login method
   Future<void> _login() async {
     try {
-      // Sign in with Firebase Authentication using email and password
       UserCredential userCredential = await _auth.signInWithEmailAndPassword(
         email: _emailController.text,
         password: _passwordController.text,
       );
 
-      // On successful login
       if (userCredential.user != null) {
-        // Navigate to a different screen (Home screen or Dashboard)
         print('Login Successful');
-        // You can use Navigator.push to navigate to the next screen
-        // Example: Navigator.pushReplacementNamed(context, '/home');
+        // Show success message
+        _showMessageBox('Login Successful', Colors.blue);
+        // Navigate to home screen
+        // Navigator.pushReplacementNamed(context, '/home');
       }
     } catch (e) {
       print('Login failed: $e');
-      // Optionally, show an error message to the user
-      showDialog(
-        context: context,
-        builder: (context) => AlertDialog(
-          title: Text('Error'),
-          content: Text('Login failed: $e'),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: Text('OK'),
-            ),
-          ],
-        ),
-      );
+      _showMessageBox('Login failed: $e', Colors.red);
     }
+  }
+
+  // Method to show the message box
+  void _showMessageBox(String message, Color color) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        backgroundColor: color,
+        title: Text('Message', style: TextStyle(color: Colors.white)),
+        content: Text(message, style: TextStyle(color: Colors.white)),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Text(
+              'OK',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Login')),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // Email Text Field
-            TextField(
-              controller: _emailController,
-              decoration: InputDecoration(labelText: 'Email'),
-            ),
-            // Password Text Field with Show/Hide Button
-            TextField(
-              controller: _passwordController,
-              obscureText: !_isPasswordVisible, // Toggle password visibility
-              decoration: InputDecoration(
-                labelText: 'Password',
-                suffixIcon: IconButton(
-                  icon: Icon(
-                    _isPasswordVisible
-                        ? Icons.visibility
-                        : Icons.visibility_off,
+      backgroundColor: Colors.purple[50], // Purple background
+      appBar: AppBar(
+        title: Text('Login', style: TextStyle(fontWeight: FontWeight.bold)),
+        backgroundColor: Colors.purple, // Purple color for AppBar
+        elevation: 0,
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Welcome Back!',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.purple, // Purple color for text
                   ),
-                  onPressed: () {
-                    setState(() {
-                      _isPasswordVisible = !_isPasswordVisible;
-                    });
-                  },
                 ),
-              ),
+                SizedBox(height: 30),
+                // Email Text Field
+                TextField(
+                  controller: _emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    labelStyle: TextStyle(color: Colors.purple), // Purple label
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide.none,
+                    ),
+                    contentPadding: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+                  ),
+                ),
+                SizedBox(height: 20),
+                // Password Text Field with Show/Hide Button
+                TextField(
+                  controller: _passwordController,
+                  obscureText: !_isPasswordVisible,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    labelStyle: TextStyle(color: Colors.purple), // Purple label
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide.none,
+                    ),
+                    contentPadding: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+                    suffixIcon: IconButton(
+                      icon: Icon(
+                        _isPasswordVisible ? Icons.visibility : Icons.visibility_off,
+                        color: Colors.purple, // Purple icon color
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _isPasswordVisible = !_isPasswordVisible;
+                        });
+                      },
+                    ),
+                  ),
+                ),
+                SizedBox(height: 30),
+                // Login Button
+                ElevatedButton(
+                  onPressed: _login,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.purple, // Purple button color
+                    padding: EdgeInsets.symmetric(vertical: 15, horizontal: 80),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  child: Text(
+                    'Login',
+
+                    style: TextStyle(
+                        color :Colors.white,
+                        fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                SizedBox(height: 20),
+                // Sign Up Text
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Don\'t have an account?'),
+                    TextButton(
+                      onPressed: () {
+                        // Navigate to SignUp screen
+                        // Navigator.pushNamed(context, '/signUp');
+                      },
+                      child: Text(
+                        'Sign Up',
+                        style: TextStyle(color: Colors.deepPurple),
+                        // Purple color
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _login,
-              child: Text('Login'),
-            ),
-          ],
+          ),
         ),
       ),
     );
